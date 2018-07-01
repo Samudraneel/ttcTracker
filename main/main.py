4bda8d65e683b77569947015045f5835c1576d24
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 00:48:18 -0400

    Started the initial set up

---
+++ main/main.py
@@ -0,0 +1,12 @@
+# PySpark implementation will be done here
+from pyspark import SparkContext, SparkConf
+
+# Initialize SparkContext
+conf = SparkConf().setAppName("Test App")
+sc = SparkContext(conf=conf)
+
+# Read data
+_dir = '~/myProjs/data/ttc/2018-06-29/'
+
+stopWords = sc.textFile(_dir + 'stop_times.txt')
+tripWords = sc.textFile(_dir + 'trips.txt')
