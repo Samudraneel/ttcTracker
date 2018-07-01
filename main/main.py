e1e2a94b7c4ea33cf671948da97cbf0470db8eda
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 18:20:16 -0400

    Checking if labels is set up correctly

--- main/main.py
+++ main/main.py
@@ -29,7 +29,7 @@ stops = stopData.filter(lambda lines: lines != header)\
         .map(lambda lines: lines.split(','))\
         .map(lambda lines: (lines[0], unix_convert(lines[1]), lines[3]))
 
-stops.saveAsTextFile('/data/output/stop')
+#stops.saveAsTextFile('/data/output/stop')
 # ----------------------------------------------------------- #
 
 # --------------------- trip data setup --------------------- #
@@ -39,13 +39,13 @@ trips = tripData.filter(lambda lines: lines != header)\
         .map(lambda lines: lines.split(','))\
         .map(lambda lines: (lines[2], lines[0]))
 
-trips.saveAsTextFile('data/output/trips')
+#trips.saveAsTextFile('data/output/trips')
 # ----------------------------------------------------------- #
 
 
 #features = map(lambda stop, trip: (stop[0], stop[2], trip.myLookup(stop, "No route ID")), stops, tripMap)
-#labels = stops.map(lambda stop: stop[1])
+labels = stops.map(lambda stop: stop[1])
 
 #features.saveAsTextFile('/data/output/features')
-#labels.saveAsTextFile('/data/output/labels')
+labels.saveAsTextFile('/data/output/labels')
 
