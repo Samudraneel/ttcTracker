02ee59dc7cf44945436d856e443b5d4a05258c3e
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 12:54:08 -0400

    Verified my spark cluster is running

--- main/main.py
+++ main/main.py
@@ -10,3 +10,6 @@ _dir = '~/myProjs/data/ttc/2018-06-29/'
 
 stopWords = sc.textFile(_dir + 'stop_times.txt')
 tripWords = sc.textFile(_dir + 'trips.txt')
+
+stopWords.saveAsTextFile('/data/output/stop_times')
+tripWords.saveAsTextFile('/data/output/trips')
