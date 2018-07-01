763a4b2e32e5ff3dca7936804d2027e97ec917d1
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 18:02:23 -0400

    Debugging

--- main/main.py
+++ main/main.py
@@ -25,15 +25,15 @@ def trueLookup(arr, err):
 # --------------------- stop data setup ---------------------- #
 
 header = stopData.first()
-dataLines = stopData.filter(lambda lines: lines != header)
-dataSplit = dataLines.map(lambda lines: lines.split(','))
+dataLines = stopData.filter(lambda lines: lines != header).map(lambda lines: lines.split(',').map(lambda lines: (lines[0], lines[1], lines[3]))
+#dataSplit = dataLines.map(lambda lines: lines.split(','))
 
 
 
 # Get data for stops
 
 # tripID, arrivalID, stopID
-stops = dataSplit.map(lambda lines: (lines[0], lines[1], lines[3]))
+#stops = dataSplit.map(lambda lines: (lines[0], lines[1], lines[3]))
 # ----------------------------------------------------------- #
 
 # --------------------- trip data setup --------------------- #
@@ -48,7 +48,7 @@ trips = dataSplit.map(lambda lines: (lines[2], lines[0]))
 #tripMap = sc.broadcast(trips.collectAsMap)
 # ----------------------------------------------------------- #
 
-stops.saveAsTextFile('/data/output/stop')
+dataLines.saveAsTextFile('/data/output/stop')
 trips.saveAsTextFile('/data/output/trips')
 
 #features = map(lambda stop, trip: (stop[0], stop[2], trip.myLookup(stop, "No route ID")), stops, tripMap)
