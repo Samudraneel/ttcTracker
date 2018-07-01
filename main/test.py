909f247e5ef7b7d793bb4479be8530aa79fe9a2a
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 15:56:24 -0400

    Testing

--- main/test.py
+++ main/test.py
@@ -4,7 +4,10 @@ from pyspark import SparkContext
 
 sc = SparkContext.getOrCreate()
 data = sc.textFile('/data/ttc/2018-06-29/stop_times.txt')
+header = data.first()
+
+
 lines = data.map(lambda line: line.split(','))
 line = lines.map(lambda val: (val[0], val[1]))
 
-lines.saveAsTextFile('/data/output/xd')
+header.saveAsTextFile('/data/output/xd')
