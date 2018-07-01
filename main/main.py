fc89b42b41cfee38525837672b655a3749cbdad6
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 19:22:15 -0400

    Creating features

--- main/main.py
+++ main/main.py
@@ -50,11 +50,10 @@ trips = tripData.filter(lambda lines: lines != header)\
 #trips.saveAsTextFile('data/output/trips')
 # ----------------------------------------------------------- #
 
-features = stops.map(lambda stop: (stop[0], stop[2], trips.get(stop[0], 'No route ID found'))
+features = stops.map(lambda stop: (stop[0], stop[2], trips.get(stop[0], 'No route ID found')))
 
 #labels = stops.map(lambda stop: stop[1])\
 #        .filter(lambda line: line != None)
 
 #features.saveAsTextFile('/data/output/features')
 #labels.saveAsTextFile('/data/output/labels')
-
