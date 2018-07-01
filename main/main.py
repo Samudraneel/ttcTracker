8f13217b930d0479f81031c0af441d793ccaaeba
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-01 19:22:52 -0400

    Creating features

--- main/main.py
+++ main/main.py
@@ -55,5 +55,5 @@ features = stops.map(lambda stop: (stop[0], stop[2], trips.get(stop[0], 'No rout
 #labels = stops.map(lambda stop: stop[1])\
 #        .filter(lambda line: line != None)
 
-#features.saveAsTextFile('/data/output/features')
+features.saveAsTextFile('/data/output/features')
 #labels.saveAsTextFile('/data/output/labels')
